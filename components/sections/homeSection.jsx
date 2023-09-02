83a169e9bdcc6d666346bd7dc81d40ce23d8ad5b
Javier Charria <linkjavier@hotmail.com>
2023-09-02 02:02:59 -0500

    Aumento de la sombra.Aumento del tamano del logo principal

--- components/sections/homeSection.jsx
+++ components/sections/homeSection.jsx
@@ -6,7 +6,7 @@ const HomeSection = props => {
   const { backgroundUrl, topText, bottomText, sloganText} = props
 
   return (
-      <div id="home" className=" xl:w-11/12 xl:translate-x-20 bg-cover bg-center shadow-xl rounded-3xl xl:mt-30 mt-20 h-full" style={{ backgroundImage: `url(${backgroundUrl}`}}>
+      <div id="home" className=" xl:w-11/12 xl:translate-x-20 bg-cover bg-center shadow-2xl rounded-3xl xl:mt-30 mt-20 h-full" style={{ backgroundImage: `url(${backgroundUrl}`}}>
         <div className=" bg-gradient-to-r from-black rounded-3xl">
           <div className="flex flex-col w-full text-white xl:justify-center pt-10 pb-20 h-full">
               <div className="padding font-trebuchet-bold-italic">
